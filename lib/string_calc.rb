7de52c1803eb5de7af5a007e5cc1d49e08d9c435
Squareben1 <bengittins@live.com>
2020-03-25 17:16:39 +0000

    refactored intify

--- lib/string_calc.rb
+++ lib/string_calc.rb
@@ -3,7 +3,6 @@ class Stringcalc
     
     number1 = str.split(" ")[0].to_i
     character = str.split(" ")[1]
-    # sym = str.split(" ")[1].insert(0, '\\')
     number2 = str.split(" ")[2].to_i
 
     if character == "+"
