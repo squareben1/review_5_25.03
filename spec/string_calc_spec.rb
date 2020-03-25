058bfb91801c9eb1af7083c1a3db20d51c5b2c62
Squareben1 <bengittins@live.com>
2020-03-25 17:16:13 +0000

    refactored tests

--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -20,24 +20,30 @@ describe Stringcalc do
       end 
     end 
 
-    it 'returns array of 1 - 1 and result - 0' do
-      expect(subject.intify("1 - 1")).to eq ["1 - 1", 0]
+    describe 'subtraction' do
+      it 'returns array of 1 - 1 and result - 0' do
+        expect(subject.intify("1 - 1")).to eq ["1 - 1", 0]
+      end
+    end 
+
+    describe 'multiplaication' do
+      it 'returns array of 1 * 1 and result - 1' do 
+        expect(subject.intify("1 * 1")).to eq ["1 * 1", 1]
+      end 
+
+      it 'returns array of 5 * 10 and result - 50' do 
+        expect(subject.intify("5 * 10")).to eq ["5 * 10", 50]
+      end 
     end
 
-    it 'returns array of 1 * 1 and result - 1' do 
-      expect(subject.intify("1 * 1")).to eq ["1 * 1", 1]
-    end 
+    describe 'division' do
+      it 'returns array of 1 / 1 and result - 1' do 
+        expect(subject.intify("1 / 1")).to eq ["1 / 1", 1]
+      end
 
-    it 'returns array of 5 * 10 and result - 50' do 
-      expect(subject.intify("5 * 10")).to eq ["5 * 10", 50]
-    end 
-
-    it 'returns array of 1 / 1 and result - 1' do 
-      expect(subject.intify("1 / 1")).to eq ["1 / 1", 1]
+      it 'returns array of 10 / 2 and result - 1' do 
+        expect(subject.intify("10 / 2")).to eq ["10 / 2", 5]
+      end 
     end
-
-    it 'returns array of 10 / 2 and result - 1' do 
-      expect(subject.intify("10 / 2")).to eq ["10 / 2", 5]
-    end 
   end 
 end 
