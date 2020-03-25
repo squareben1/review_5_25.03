6e023fc7bcdf391978caf543d570998ad5c43a67
Squareben1 <bengittins@live.com>
2020-03-25 11:48:31 +0000

    confirmation testing

--- spec/string_calc_spec.rb
+++ spec/string_calc_spec.rb
@@ -22,8 +22,16 @@ describe Stringcalc do
       expect(subject.intify("1 * 1")).to eq ["1 * 1", 1]
     end 
 
+    it 'returns array of 5 * 10 and result - 50' do 
+      expect(subject.intify("5 * 10")).to eq ["5 * 10", 50]
+    end 
+
     it 'returns array of 1 / 1 and result - 1' do 
       expect(subject.intify("1 / 1")).to eq ["1 / 1", 1]
+    end
+
+    it 'returns array of 10 / 2 and result - 1' do 
+      expect(subject.intify("10 / 2")).to eq ["10 / 2", 5]
     end 
   end 
 end 
